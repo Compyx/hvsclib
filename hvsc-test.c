0f8f935c1cbd4aa2e6c12bb9961c43581f6d94c6
compyx <b.wassink@ziggo.nl>
2018-03-06 23:05:00 +0100

    Implement test-case selection in hvsc-test
    The command line structure of hvsc-test has changed: it now expects both
    a test-name and a path to a PSID file.
    If given 'all' as the test-name, it'll run all the available tests, if
    given a specific test-name, it'll run just that test.
    Run hvsc-test without arguments to get a help text.

--- hvsc-test.c
+++ hvsc-test.c
@@ -27,55 +27,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <inttypes.h>
 #include <stdbool.h>
 
 #include "hvsc.h"
 
-
-/** \brief  Test driver
- *
- * \return  EXIT_SUCCESS or EXIT_FAILURE
+/** \brief  Test case
  */
-int main(int argc, char *argv[])
+typedef struct test_case_s {
+    const char *name;               /**< test name */
+    const char *desc;               /**< test description */
+    bool (*func)(const char *);     /**< test function */
+} test_case_t;
+
+
+
+
+/** \brief  Run SLDB test on \a psid
+ *
+ * \param[in]   path    path to SID file
+ *
+ * \return  bool
+ */
+static bool test_sldb(const char *path)
 {
-    int num;
     int i;
+    int num;
     long *lengths;
 
-    hvsc_stil_t stil;
-    hvsc_bugs_t bugs;
-    hvsc_stil_tune_entry_t tune_entry;
-    hvsc_psid_t psid;
-
-    int major;
-    int minor;
-    int revision;
-
-    puts("HVSC LIB test driver\n");
-
-    printf("Library version string = %s\n", hvsc_lib_version_str());
-    hvsc_lib_version_num(&major, &minor, &revision);
-    printf("Libarry version number = %d, %d, %d\n", major, minor, revision);
-
-    if (argc < 2) {
-        printf("Usage: %s <PSID-file>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    printf("Initializing .. ");
-    if (!hvsc_init("/home/compyx/c64/HVSC")) {
-        hvsc_perror(argv[0]);
-        return EXIT_FAILURE;
-    }
-    printf("OK\n");
-
-    printf("Retrieving songlength info .. \n");
-    num = hvsc_sldb_get_lengths(argv[1], &lengths);
+    printf("Retrieving song lengths of '%s'\n", path);
+    num = hvsc_sldb_get_lengths(path, &lengths);
     if (num < 0) {
-        hvsc_perror(argv[0]);
-        hvsc_exit();
-        return EXIT_SUCCESS;
+        return false;
     }
     printf("OK: ");
     printf("Got %d songs:\n", num);
@@ -85,18 +69,30 @@ int main(int argc, char *argv[])
     }
 
     free(lengths);
+    return true;
+}
 
 
-    printf("\nTesting STIL handling\n\n");
+/** \brief  Run STIL test on \a psid
+ *
+ * \param[in]   path    path to SID file
+ *
+ * \return  bool
+ */
+static bool test_stil(const char *path)
+{
+    hvsc_stil_t stil;
+    hvsc_stil_tune_entry_t tune_entry;
 
-    printf("Opening STIL, looking for %s\n", argv[1]);
-    if (!hvsc_stil_open(argv[1], &stil)) {
-        hvsc_perror(argv[0]);
+    printf("Testing STIL handling\n\n");
+
+    printf("Opening STIL, looking for %s\n", path);
+    if (!hvsc_stil_open(path, &stil)) {
+        hvsc_perror("hvsc-test");
         if (hvsc_errno != HVSC_ERR_NOT_FOUND) {
-            hvsc_exit();
-            return EXIT_FAILURE;
+            return false;
         }
-        printf("Continuing\n");
+        printf("Continuing anyway...\n");
     } else {
 
         printf("Reading STIL entry text\n");
@@ -109,8 +105,7 @@ int main(int argc, char *argv[])
         if (!hvsc_stil_parse_entry(&stil)) {
             hvsc_perror("Failed");
             hvsc_stil_close(&stil);
-            hvsc_exit();
-            return EXIT_FAILURE;
+            return false;
         }
 
         printf("Dumping parsed data:\n");
@@ -120,6 +115,8 @@ int main(int argc, char *argv[])
         printf("\nTesting hvsc_get_tune_entry(3):\n");
         if (!hvsc_stil_get_tune_entry(&stil, &tune_entry, 3)) {
             hvsc_perror("Failed");
+            hvsc_stil_close(&stil);
+            return false;
         } else {
             printf("OK! Calling hvsc_stil_dump_tune_entry()\n\n");
             hvsc_stil_dump_tune_entry(&tune_entry);
@@ -128,28 +125,143 @@ int main(int argc, char *argv[])
         printf("Closing STIL\n");
         hvsc_stil_close(&stil);
     }
+    return true;
+}
 
-    printf("\n\nTesting HVSC BUGlist\n\n");
 
-    if (!hvsc_bugs_open(argv[1], &bugs)) {
-        hvsc_perror(argv[0]);
+/** \brief  Run BUGlist test on \a psid
+ *
+ * \param[in]   path    path to SID file
+ *
+ * \return  bool
+ */
+static bool test_buglist(const char *path)
+{
+    hvsc_bugs_t bugs;
+
+    printf("Testing HVSC BUGlist\n\n");
+
+    if (!hvsc_bugs_open(path, &bugs)) {
+        hvsc_perror("hvsc-test");
         if (hvsc_errno == HVSC_ERR_NOT_FOUND) {
             printf("BUGlist: No entry found, no worries\n");
+            return true;
         }
+        return false;
     } else {
         printf("Found entry:\n");
         printf("{ bug} %s\n", bugs.text);
         printf("{user} %s\n", bugs.user);
         hvsc_bugs_close(&bugs);
+        return true;
     }
+}
 
+
+/** \brief  Run PSID test on \a psid
+ *
+ * \param[in]   path    path to SID file
+ *
+ * \return  bool
+ */
+static bool test_psid(const char *path)
+{
+    hvsc_psid_t psid;
     printf("\n\nTesing PSID file handling\n\n");
 
-    printf("Opening %s\n", argv[1]);
-    if (hvsc_psid_open(argv[1], &psid)) {
+    printf("Opening %s\n", path);
+    if (hvsc_psid_open(path, &psid)) {
         printf("Dumping header:\n");
         hvsc_psid_dump(&psid);
         hvsc_psid_close(&psid);
+        return true;
+    } else {
+        hvsc_perror("hvsc-test");
+        return false;
+    }
+}
+
+
+/** \brief  Test cases
+ */
+static test_case_t cases[] = {
+    { "sldb", "test Songlength.* database support", test_sldb },
+    { "stil", "test STIL.txt (SID Tune Information List) support", test_stil },
+    { "bugs", "test BUGlist.txt suport", test_buglist },
+    { "psid", "test PSID file support", test_psid },
+    { NULL, NULL, NULL }
+};
+
+
+/** \brief  Print usage message on stdout
+ *
+ * \param[in]   prg program name
+ */
+static void usage(const char *prg)
+{
+    int i;
+
+    printf("Usage: %s <test-name> <psid-file>\n", prg);
+    printf("\n<test-name> can either be 'all' to run all tests, or:\n");
+    for (i = 0; cases[i].name != NULL; i++) {
+        printf("\t%s\t%s\n", cases[i].name, cases[i].desc);
+    }
+}
+
+
+
+/** \brief  Test driver
+ *
+ * \return  EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char *argv[])
+{
+    int i;
+
+    int major;
+    int minor;
+    int revision;
+
+    if (argc < 3) {
+        usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    puts("HVSC LIB test driver\n");
+
+    printf("Library version string = %s\n", hvsc_lib_version_str());
+    hvsc_lib_version_num(&major, &minor, &revision);
+    printf("Libarry version number = %d, %d, %d\n", major, minor, revision);
+
+    printf("Initializing .. ");
+    if (!hvsc_init("/home/compyx/c64/HVSC")) {
+        hvsc_perror(argv[0]);
+        return EXIT_FAILURE;
+    }
+    printf("OK\n");
+
+    printf("Got case '%s'\n", argv[1]);
+
+    if (strcmp(argv[1], "all") == 0) {
+        printf("Running all tests\n\n");
+        for (i = 0; cases[i].name != NULL; i++) {
+            if (cases[i].func(argv[2])) {
+                printf("<<OK>>\n");
+            } else {
+                printf("<<Fail>>\n");
+            }
+        }
+    } else {
+        for (i = 0; cases[i].name != NULL; i++) {
+            if (strcmp(cases[i].name, argv[1]) == 0) {
+                if (cases[i].func(argv[2])) {
+                    printf("<<OK>>\n");
+                } else {
+                    printf("<<Fail>>\n");
+                }
+                break;
+            }
+        }
     }
 
     hvsc_exit();
